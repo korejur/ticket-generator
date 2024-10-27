25b1b94881d27c0c178cb3a11bd9969f634d767a
kore <korinajuric03@gmail.com>
2024-10-27 15:27:05 +0100

    Update auth0.ts

--- backend/src/config/auth0.ts
+++ backend/src/config/auth0.ts
@@ -2,7 +2,7 @@ export const auth0Config = {
   authRequired: false,
   auth0Logout: true,
   secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://ticket-generator-7cbn.onrender.com',
   clientID: 'JIlXfUeHPnu8lVyFbaQ6MTsrWebyzeZl',
   issuerBaseURL: 'https://dev-746naqxvi6sm0ikc.eu.auth0.com',
 };
